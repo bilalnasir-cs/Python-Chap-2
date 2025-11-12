7576260a528b6a04651ef6ea9a6695070fb6daa3
bilalnasir-cs <bilal.nasir@gahic.edu.pk>
2025-11-12 11:26:20 +0500

    update variables22.py

--- variablesPg22.py
+++ variablesPg22.py
@@ -2,3 +2,23 @@ age = 71
 print("Ahmed lived for ",age,"years")
 age = 60
 print("Iqbal lived for ",age," years")
+
+#Varible Naming Rules
+#1.
+number = 1
+Number = 2
+_Number = 3
+
+#Case Sensitive
+age = 10
+Age = 11
+
+#Not allowed
+#if = 1
+
+#Data Types
+
+age = 17
+price = 19.99
+name = "Ali"
+is_Student = True
