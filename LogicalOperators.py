cc99bc728ca5513f7fe013a12b71648b849a9e8f
bilalnasir-cs <bilal.nasir@gahic.edu.pk>
2025-11-12 18:14:47 +0500

    Page 24

---
+++ LogicalOperators.py
@@ -0,0 +1,15 @@
+#Define variables
+x = True
+y = False
+
+#Logical AND
+logical_and = x and y
+print(x,"and",y,"=",logical_and)
+
+#Logical OR
+logical_or = x or y
+print(x,"or",y,"=",logical_or)
+
+#Logical NOT
+logical_not = not x
+print(x,", not",x,"=",logical_not)
